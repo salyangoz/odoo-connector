581d0f0532a3802f9e8db82c528bd12e09a72c06
Salyangoz <info@salyangoz.com.tr>
2026-06-18 16:00:41 +0300

    Update redirect URL to app.yengec.co/auth/callback/odoo

--- yengec_connector/controllers/main.py
+++ yengec_connector/controllers/main.py
@@ -2,7 +2,7 @@ import urllib.parse
 from odoo import http
 from odoo.http import request
 
-YENGEC_SETUP_URL = 'https://app.yengec.co/login/odoo'
+YENGEC_SETUP_URL = 'https://app.yengec.co/auth/callback/odoo'
 YENGEC_API_KEY_PARAM = 'yengec_connector.api_key'
 
 
